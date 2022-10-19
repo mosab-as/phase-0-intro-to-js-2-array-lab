df0f16dc1b314b1671e41063a720f96917eba98b
Golden-Man <Golden-Man@users.noreply.github.com>
2022-10-19 18:12:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+const destructivelyAppendCat = (catName)=> cats.push(catName);
+const destructivelyPrependCat = (catName)=> cats.unshift(catName);
+const destructivelyRemoveLastCat = ()=> cats.pop();
+const destructivelyRemoveFirstCat = ()=> cats.shift();
+const appendCat = (catName)=> [...cats,catName];
+const prependCat = (catName)=> [catName,...cats];
+const removeLastCat = ()=> cats.slice(0,cats.length-1);
+const removeFirstCat = ()=> cats.slice(1);
